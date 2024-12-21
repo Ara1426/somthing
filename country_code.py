e34b039f6ce0cee76e7e32fe0f4e871207ed5fed
Ara1426 <dilanlateef@gmail.com>
2024-12-21 19:09:02 +0300

    Add files via upload

---
+++ country_code.py
@@ -0,0 +1,7 @@
+codes={'iraq':'964','india':'0091','hungary':'36'}
+
+print("country code for iraq")
+print(codes.get('iraq','not found'))
+
+print("country code for china")
+print(codes.get('china','not found'))
---
+++ frequency.py
@@ -0,0 +1,12 @@
+x={'ara':6,'is':6,'the':6,'best':6}
+print(x)
+
+a=6
+
+count=0
+
+for key in x:
+    if x[key]==a:
+        count=count+1
+
+print(count)
